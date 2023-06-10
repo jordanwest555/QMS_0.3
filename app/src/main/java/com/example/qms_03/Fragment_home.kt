211774d0a496431ec0ee6260e05c91709a115b63
jordanwest555 <jordanvdwest@gmail.com>
2023-06-10 15:20:12 +0200

    Uploading to git

--- app/src/main/java/com/example/qms_03/Fragment_home.kt
+++ app/src/main/java/com/example/qms_03/Fragment_home.kt
@@ -3,3 +3,4 @@ package com.example.qms_03
 import androidx.fragment.app.Fragment
 
 class Fragment_home : Fragment()
+
--- app/src/main/java/com/example/qms_03/MenuFragment.kt
+++ app/src/main/java/com/example/qms_03/MenuFragment.kt
@@ -1,10 +1,13 @@
 package com.example.qms_03
 
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.qms_03.R.layout.fragment_menu
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +19,15 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MenuFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MenuFragment : Fragment() {
+class MenuFragment() : Fragment(), Parcelable {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    constructor(parcel: Parcel) : this() {
+        param1 = parcel.readString()
+        param2 = parcel.readString()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +42,7 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        return inflater.inflate(fragment_menu, container, false)
     }
 
     companion object {
@@ -56,4 +64,90 @@ class MenuFragment : Fragment() {
                 }
             }
     }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(param1)
+        parcel.writeString(param2)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MenuFragment> {
+        override fun createFromParcel(parcel: Parcel): MenuFragment {
+            return MenuFragment(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MenuFragment?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+class MenuFragment() : Fragment(), Parcelable {
+
+    private lateinit var logQueryButton: Button
+    private lateinit var queryStatusButton: Button
+    private lateinit var queryHistoryButton: Button
+    private lateinit var rateQmsButton: Button
+
+    constructor(parcel: Parcel) : this() {
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_menu, container, false)
+
+        logQueryButton = view.findViewById(R.id.log_query_button)
+        queryStatusButton = view.findViewById(R.id.query_status_button)
+        queryHistoryButton = view.findViewById(R.id.query_history_button)
+        rateQmsButton = view.findViewById(R.id.rate_qms_button)
+
+        logQueryButton.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_logQueryFragment)
+        }
+
+        queryStatusButton.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_queryStatusFragment)
+        }
+
+        queryHistoryButton.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_queryHistoryFragment)
+        }
+
+        rateQmsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_rateQmsFragment)
+        }
+
+        return view
+    }
+
+    private fun findNavController(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MenuFragment> {
+        override fun createFromParcel(parcel: Parcel): MenuFragment {
+            return MenuFragment(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MenuFragment?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+class Button {
+
 }
