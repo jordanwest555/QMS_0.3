0c5275644ecbff66d6cd8b884fd22235f97e4dc9
zeppy <65682732+zeppyOG@users.noreply.github.com>
2023-07-05 10:14:55 +0200

    auto navigate to login page after successfull signup

--- app/src/main/java/com/example/qms_03/Fragment_sign_up.kt
+++ app/src/main/java/com/example/qms_03/Fragment_sign_up.kt
@@ -6,9 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.qms_03.databinding.FragmentSignUpBinding
 import android.util.Log
-
+import org.mindrot.jbcrypt.BCrypt
 
 class Fragment_sign_up : Fragment() {
     private var _binding: FragmentSignUpBinding? = null
@@ -39,10 +40,12 @@ class Fragment_sign_up : Fragment() {
             val isAdmin = binding.adminCheckbox.isChecked
 
             if (name.isNotBlank() && email.isNotBlank() && password.isNotBlank() && password == confirmPassword) {
+                val hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt())
                 val dbHelper = DatabaseHelper(requireContext())
-                dbHelper.insertUser(User(0, email, "", name, "", isAdmin))
+                dbHelper.insertUser(User(null, email, hashedPassword, "", name, "", isAdmin))
                 Toast.makeText(requireContext(), "User registered successfully", Toast.LENGTH_SHORT).show()
-                //printAllUsers(dbHelper)   -- test code to verify database working, can be viewed in logcat.
+                printAllUsers(dbHelper)   //test code to verify database working, can be viewed in logcat.
+                findNavController().navigate(R.id.action_Fragment_sign_up_to_Fragment_login)
             } else {
                 Toast.makeText(requireContext(), "Please fill in all fields or make sure your passwords match", Toast.LENGTH_SHORT).show()
             }
--- app/src/main/java/com/example/qms_03/database.kt
+++ app/src/main/java/com/example/qms_03/database.kt
@@ -4,10 +4,12 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import org.mindrot.jbcrypt.BCrypt
 
 data class User(
     val userID: Int?,
     val email: String,
+    val password: String,
     val studentID: String,
     val firstName: String,
     val lastName: String,
@@ -17,12 +19,13 @@ data class User(
 class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
         private const val DATABASE_NAME = "database.db"
-        private const val DATABASE_VERSION = 3
+        private const val DATABASE_VERSION = 4 // Updated to 4
 
         // Define the table and column names
         private const val TABLE_NAME = "User"
         private const val COLUMN_USER_ID = "userID"
         private const val COLUMN_EMAIL = "email"
+        private const val COLUMN_PASSWORD = "password"
         private const val COLUMN_STUDENT_ID = "studentID"
         private const val COLUMN_FIRST_NAME = "firstName"
         private const val COLUMN_LAST_NAME = "lastName"
@@ -34,6 +37,7 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
             CREATE TABLE $TABLE_NAME (
                 $COLUMN_USER_ID INTEGER PRIMARY KEY AUTOINCREMENT,
                 $COLUMN_EMAIL TEXT,
+                $COLUMN_PASSWORD TEXT,
                 $COLUMN_STUDENT_ID TEXT,
                 $COLUMN_FIRST_NAME TEXT,
                 $COLUMN_LAST_NAME TEXT,
@@ -44,7 +48,7 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        if (oldVersion < 3) {
+        if (oldVersion < 4) { // Updated to 4
             db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
             onCreate(db)
         }
@@ -52,8 +56,10 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
 
     fun insertUser(user: User) {
         val db = writableDatabase
+        val hashedPassword = BCrypt.hashpw(user.password, BCrypt.gensalt())
         val values = ContentValues().apply {
             put(COLUMN_EMAIL, user.email)
+            put(COLUMN_PASSWORD, hashedPassword)
             put(COLUMN_STUDENT_ID, user.studentID)
             put(COLUMN_FIRST_NAME, user.firstName)
             put(COLUMN_LAST_NAME, user.lastName)
@@ -62,6 +68,30 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
         db.insert(TABLE_NAME, null, values)
         db.close()
     }
+
+    fun validateUser(email: String, password: String): User? {
+        val db = readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM $TABLE_NAME WHERE $COLUMN_EMAIL = ?", arrayOf(email))
+        if (cursor.moveToFirst()) {
+            val hashedPassword = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_PASSWORD))
+            if (BCrypt.checkpw(password, hashedPassword)) {
+                val user = User(
+                    cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_USER_ID)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_EMAIL)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_PASSWORD)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_STUDENT_ID)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_FIRST_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_LAST_NAME)),
+                    cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_IS_ADMIN)) == 1
+                )
+                cursor.close()
+                return user
+            }
+        }
+        cursor.close()
+        return null
+    }
+
 
     fun getAllUsers(): List<User> {
         val db = readableDatabase
@@ -72,47 +102,7 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
                 val user = User(
                     it.getInt(it.getColumnIndexOrThrow(COLUMN_USER_ID)),
                     it.getString(it.getColumnIndexOrThrow(COLUMN_EMAIL)),
-                    it.getString(it.getColumnIndexOrThrow(COLUMN_STUDENT_ID)),
-                    it.getString(it.getColumnIndexOrThrow(COLUMN_FIRST_NAME)),
-                    it.getString(it.getColumnIndexOrThrow(COLUMN_LAST_NAME)),
-                    it.getInt(it.getColumnIndexOrThrow(COLUMN_IS_ADMIN)) == 1
-                )
-                userList.add(user)
-            }
-        }
-        db.close()
-        return userList
-    }
-
-    fun getUserByID(userID: Int): List<User> {
-        return getUsersByCondition("$COLUMN_USER_ID = $userID")
-    }
-
-    fun getUserByEmail(email: String): List<User> {
-        return getUsersByCondition("$COLUMN_EMAIL = '$email'")
-    }
-
-    fun getUserByStudentID(studentID: String): List<User> {
-        return getUsersByCondition("$COLUMN_STUDENT_ID = '$studentID'")
-    }
-
-    fun getUserByFirstName(firstName: String): List<User> {
-        return getUsersByCondition("$COLUMN_FIRST_NAME = '$firstName'")
-    }
-
-    fun getUserByLastName(lastName: String): List<User> {
-        return getUsersByCondition("$COLUMN_LAST_NAME = '$lastName'")
-    }
-
-    private fun getUsersByCondition(condition: String): List<User> {
-        val db = readableDatabase
-        val cursor = db.rawQuery("SELECT * FROM $TABLE_NAME WHERE $condition", null)
-        val userList = mutableListOf<User>()
-        cursor.use {
-            while (it.moveToNext()) {
-                val user = User(
-                    it.getInt(it.getColumnIndexOrThrow(COLUMN_USER_ID)),
-                    it.getString(it.getColumnIndexOrThrow(COLUMN_EMAIL)),
+                    it.getString(it.getColumnIndexOrThrow(COLUMN_PASSWORD)),
                     it.getString(it.getColumnIndexOrThrow(COLUMN_STUDENT_ID)),
                     it.getString(it.getColumnIndexOrThrow(COLUMN_FIRST_NAME)),
                     it.getString(it.getColumnIndexOrThrow(COLUMN_LAST_NAME)),
